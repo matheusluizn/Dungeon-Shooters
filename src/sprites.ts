35cd7880b8458b316f3e4888729b541b0d2bc8b1
mt <matheusluizn@gmail.com>
2022-06-11 18:16:00 -0300

    feat: creating sprites module

---
+++ src/sprites.ts
@@ -0,0 +1,19 @@
+import mage_sprite from "../assets/sprites/main-character/wizard-idle.png";
+import mage_projectile_sprite from "../assets/sprites/projectile/mage_projectile.png";
+
+import elf_sprite from "../assets/sprites/main-character/elf-idle.png";
+import elf_projectile_sprite from "../assets/sprites/projectile/elf_projectile.png";
+
+
+const SPRITES = {
+    mage: {
+        sprite: mage_sprite,
+        projectile: mage_projectile_sprite,
+    },
+    elf: {
+        sprite: elf_sprite,
+        projectile: elf_projectile_sprite,
+    },
+}
+
+export default SPRITES;
