536e1ab16c2e98e1f3a7d9caaff2eb41ac04127c
mt <matheusluizn@gmail.com>
2022-06-11 16:42:34 -0300

    feat: creating player class

---
+++ src/classes/Player.ts
@@ -0,0 +1,9 @@
+import GameObject from "./GameObject";
+
+class Player extends GameObject{
+    constructor(x : number, y : number, width : number, height : number, image : string, context : CanvasRenderingContext2D){
+        super(x, y, width, height, image, context);
+    }
+}
+
+export default Player;
