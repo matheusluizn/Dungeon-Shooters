dbd1e6a523d0a2d21b9d6af8acda9e5c1cd1f928
mt <matheusluizn@gmail.com>
2022-06-11 21:20:35 -0300

    feat: creating smooth player movement

--- src/classes/Player.ts
+++ src/classes/Player.ts
@@ -1,9 +1,27 @@
 import GameObject from "./GameObject";
 
 class Player extends GameObject {
-    constructor(x: number, y: number, width: number, height: number, image: string, context: CanvasRenderingContext2D) {
+    speed: number;
+    constructor(x: number, y: number, width: number, height: number, image: string, context: CanvasRenderingContext2D, speed: number) {
         super(x, y, width, height, image, context);
+        this.speed = speed;
     }
+
+    update(keysDown: any, modifier: number) {
+        if (keysDown.ArrowLeft) {
+            this.x -= this.speed * modifier;
+        }
+        if (keysDown.ArrowRight) {
+            this.x += this.speed * modifier;
+        }
+        if (keysDown.ArrowUp) {
+            this.y -= this.speed * modifier;
+        }
+        if (keysDown.ArrowDown) {
+            this.y += this.speed * modifier;
+        }
+    }
+
 }
 
 export default Player;
